4a35977972d2c2e8eaeb51d70ae140892ed2e8e7
Jonathan Wilson <gogs@fake.local>
2021-01-05 17:18:14 +0000

    Changes to rename sample plugin

---
+++ src/main/java/lightkeeper/LightKeeperProvider.java
@@ -0,0 +1,54 @@
+package lightkeeper;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import docking.ActionContext;
+import docking.ComponentProvider;
+import docking.action.DockingAction;
+import docking.action.ToolBarData;
+import ghidra.framework.plugintool.Plugin;
+import ghidra.util.Msg;
+import resources.Icons;
+
+public class LightKeeperProvider extends ComponentProvider {
+
+	private JPanel panel;
+	private DockingAction action;
+
+	public LightKeeperProvider(Plugin plugin, String owner) {
+		super(plugin.getTool(), owner, owner);
+		buildPanel();
+		createActions();
+	}
+
+	private void buildPanel() {
+		panel = new JPanel(new BorderLayout());
+		JTextArea textArea = new JTextArea(5, 25);
+		textArea.setEditable(false);
+		panel.add(new JScrollPane(textArea));
+		setVisible(true);
+	}
+
+	private void createActions() {
+		action = new DockingAction("About", getName()) {
+			@Override
+			public void actionPerformed(ActionContext context) {
+				Msg.showInfo(getClass(), panel, "About", "Light Keeper");
+			}
+		};
+		action.setToolBarData(new ToolBarData(Icons.ADD_ICON, null));
+		action.setEnabled(true);
+		action.markHelpUnnecessary();
+		dockingTool.addLocalAction(this, action);
+	}
+
+	@Override
+	public JComponent getComponent() {
+		return panel;
+	}
+}
